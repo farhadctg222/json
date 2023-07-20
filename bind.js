8087a279660415704111fb4543a0a00321f36194
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-20 11:18:20 -0700

    de

--- bind.js
+++ bind.js
@@ -6,12 +6,9 @@ const student = {
    adrees: 'chattogram',
    edu: 'S.S.C',
    salary: 25000,
-   fullName: function(){
    
-    console.log( this.fistName,this.lastName)
-   },
    charchBill(amount){
-    console.log(this)
+    
     this.salary = this.salary-amount
     return this.salary
    }
@@ -31,20 +28,26 @@ const masud = {
     lastName: 'uddin',
     salary: 50000
 }
-// student.charchBill()
+student.charchBill()
 const fnd =student.charchBill.bind(saluddn)
 fnd(2154)
 fnd(250)
 fnd(530)
-// console.log(saluddn.salary)
-// console.log(student.salary)
+console.log(saluddn.salary)
+console.log(student.salary)
 
 const masudSalary = student.charchBill.bind(masud)
 masudSalary(20)
 masudSalary(3840)
 console.log(masudSalary(40))
 
+student.charchBill.call(masud,300)
+student.charchBill.call(masud,2100)
+
+console.log(masud.salary)
+console.log(masud)
 
 
-
+student.charchBill.apply(masud,[300])
+console.log(masud,masud.salary)
 
