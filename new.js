80ebb0c9aa9d473122a6beadf6de293ecaac3b65
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-20 11:44:19 -0700

    d

--- new.js
+++ new.js
@@ -7,3 +7,10 @@ class person{
 }
 const hero  = new person('hero','alam',2000)
 console.log(hero)
+
+function name(fistName,lastName,salary){
+    this.fistName = fistName;
+    this.lastName = lastName;
+    this.salary= salary;
+}
+console.log(new name('farhad','uddin',2000))
