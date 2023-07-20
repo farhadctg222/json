d59c89fde454aa1d969a355770544c2df0887b1e
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-20 08:59:42 -0700

    waa

--- bind.js
+++ bind.js
@@ -5,13 +5,19 @@ const student = {
    lastName: 'uddin',
    adrees: 'chattogram',
    edu: 'S.S.C',
+   salary: 5000,
    fullName: function(){
     this.fistName
     this.lastName
     console.log(this.fullName)
+   },
+   charchBill(amount){
+    this.salary = amount - this.salary
+    return amount
    }
 
 }
-console.log('hello world')
+student.charchBill(200)
+console.log(student.charchBill())
 
 
