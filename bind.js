1ba967970a0d3a93533bf787ff0186e800a46c1b
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-20 08:45:45 -0700

    d

---
+++ bind.js
@@ -0,0 +1,17 @@
+
+
+const student = {
+   fistName: 'farhad',
+   lastName: 'uddin',
+   adrees: 'chattogram',
+   edu: 'S.S.C',
+   fullName: function(){
+    this.fistName
+    this.lastName
+    console.log(this.fullName)
+   }
+
+}
+console.log('hello world')
+
+
